41868bfad289252a9e615659a6ed93c82b307287
Kaznacheykin <danya.kaznach@mail.ru>
2025-04-06 22:44:53 +0300

    Update video page dimensions

--- resources/views/public/video.blade.php
+++ resources/views/public/video.blade.php
@@ -20,13 +20,13 @@ use Illuminate\Support\Facades\Config;
                     <div class="mt-8 flex flex-col justify-center center items-center" id = "frame" data-tkn = "{{$tkn}}" data-id="{{$video[VideoContents::FIELD_ID]}}" data-testurl = "{{ base64_encode($video[VideoContents::FIELD_URL]) }}">
                     </div>
                     <div class="p-5 flex justify-between">
-                        <div class="flex items-start text-blue-500">
+                        <div class="flex items-start text-blue-500 gap-x-2">
                             <svg class="h-5 w-5 text-blue-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"/>
                             </svg>
                             {{$video[VideoContents::FIELD_LIKES]}}
                         </div>
-                        <div class="flex items-start text-blue-500">
+                        <div class="flex items-start text-blue-500 gap-x-2">
                             <svg class="h-5 w-5 text-blue-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"/>
