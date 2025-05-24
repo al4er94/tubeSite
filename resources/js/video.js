4f39d62a2f757fee3c44374b3871abb84540449b
Shevch <shevch19944@gmail.com>
2025-05-24 17:48:25 +0300

    use https

--- resources/js/video.js
+++ resources/js/video.js
@@ -9,6 +9,6 @@ window.onload = function() {
 	var token = iframe.dataset.tkn
 	var id = iframe.dataset.id
 	var testurl = iframe.dataset.testurl
-	var url = "http://79.132.139.76:81/test?v=" + testurl + "&width=" + frameWidth +"&height=" + frameHeight + "&tkn=" + token;
+	var url = "https://www.leaked-girls.com:81/test?v=" + testurl + "&width=" + frameWidth +"&height=" + frameHeight + "&tkn=" + token;
     iframe.innerHTML = '<iframe width="' + frameWidth + '" height="' + frameHeight + '" src="'+ url +'" frameborder="0" allowfullscreen></iframe>'
 }
